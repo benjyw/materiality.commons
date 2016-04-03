6cec494455dda406c2d0c9d375cbeb65e7f3a13f
Benjy <benjyw@gmail.com>
2016-04-03 13:35:36 -0700

    Support the --insecure flag to runserver.
    Switches our run_dev_server script to use argparse, for convenience.

--- src/python/materiality/util/run_dev_server.py
+++ src/python/materiality/util/run_dev_server.py
@@ -4,21 +4,23 @@
 from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
                         print_function, unicode_literals)
 
+import argparse
 import socket
-import sys
 
 from colors import blue, green, red
 
 from materiality.util.execute import execute, execute_postgres
 
 
-def run_dev_server():
+def run_dev_server(args):
   print(green('Starting local postgres server.'))
   execute_postgres('start')
   try:
     print(green('\nStarting local webapp.'))
     cmd = './venv/bin/python manage.py runserver'
-    if with_remote_access:
+    if args.insecure:
+      cmd += ' --insecure'
+    if args.with_remote_access:
       cmd += ' 0.0.0.0:8000'
       my_host = socket.gethostbyname(socket.gethostname())
       print(blue('\nAccess remotely at http://{0}:8000'.format(my_host)))
@@ -34,12 +36,15 @@ def run_dev_server():
   print('\n')
 
 
+def main():
+  parser = argparse.ArgumentParser(description='Run a dev server for a materiality-based app.')
+  parser.add_argument('--with-remote-access', action='store_true',
+                      help='Allow non-localhost access (e.g., for testing from a mobile device).')
+  parser.add_argument('--insecure', action='store_true',
+                      help='Allow insecure access (e.g., for serving static files while testing with DEBUG=False).')
+  args = parser.parse_args()
+  run_dev_server(args)
+
+
 if __name__ == '__main__':
-  with_remote_access = False
-  if len(sys.argv) > 1:
-    if len(sys.argv) == 2 and sys.argv[1] == '--with-remote-access':
-      with_remote_access = True
-    else:
-      print(red('Unknown options: {0}'.format(' '.join(sys.argv[1:]))))
-      sys.exit(1)
-  run_dev_server()
+  main()
