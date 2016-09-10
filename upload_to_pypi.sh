8d816593cfaa698d118935442ef8e78538a0c422
Benjy <benjyw@gmail.com>
2016-09-09 17:51:34 -0700

    Create a git tag for each pypi push.

--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -2,3 +2,7 @@
 # See http://peterdowns.com/posts/first-time-with-pypi.html
 python setup.py register -r pypi
 python setup.py sdist upload -r pypi
+VERSION=`python setup.py -V`
+VERSION_TAG="v${VERSION}"
+git tag "${VERSION_TAG}"
+git push origin "${VERSION_TAG}"
